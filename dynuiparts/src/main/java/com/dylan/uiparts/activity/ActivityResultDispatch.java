17e1a813eb13ed17d75680cc64e2ac6aea65c248
Dylan <dylan@192.168.110.92>
2020-08-18 17:32:33 +0800

    no message

--- dynuiparts/src/main/java/com/dylan/uiparts/activity/ActivityResultDispatch.java
+++ dynuiparts/src/main/java/com/dylan/uiparts/activity/ActivityResultDispatch.java
@@ -59,6 +59,31 @@ public class ActivityResultDispatch {
         return false;
     }
 
+    public static boolean onActivityResult(Object fragment, int requestCode, int resultCode, Intent data) {
+        Method executeMethod = findMethodWithRequestCode(fragment.getClass(), requestCode);
+        if(executeMethod != null){
+            try {
+                if(!executeMethod.isAccessible()) {
+                    executeMethod.setAccessible(true);
+                }
+                Class<?>[] parameterTypes = executeMethod.getParameterTypes();
+                if (parameterTypes.length == 3) {
+                    executeMethod.invoke(fragment, requestCode, resultCode, data);
+                } else if (parameterTypes.length == 2) {
+                    executeMethod.invoke(fragment, resultCode, data);
+                } else if (parameterTypes.length == 1 && resultCode == Activity.RESULT_OK) {
+                    executeMethod.invoke(fragment, data);
+                } else if (parameterTypes.length == 0 && resultCode == Activity.RESULT_OK) {
+                    executeMethod.invoke(fragment, new Object[]{});
+                }
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
     private static <A extends Annotation> Method findMethodWithRequestCode(Class clazz, int requestCode) {
         try {
             for (; clazz != null; clazz = clazz.getSuperclass()) {
